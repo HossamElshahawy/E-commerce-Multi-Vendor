0e6a1d247d0de641262700ba512bd1e15a92206f
HossamElshahawy <hossamelshahawy13@gmail.com>
2023-09-01 17:58:34 +0300

    make factory and seeder for users

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Testing\Fluent\Concerns\Has;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+            //admin
+            'full_name'=>'hossam Admin',
+            'username'=>'Admin',
+            'email'=>'admin@gmail.com',
+            'password'=>Hash::make('admin'),
+            'role'=>'admin',
+            'status'=>'active'
+            ],
+            [
+                //vendor
+                'full_name'=>'hossam vendor',
+                'username'=>'vendor',
+                'email'=>'vendor@gmail.com',
+                'password'=>Hash::make('vendor'),
+                'role'=>'vendor',
+                'status'=>'active'
+            ],
+            //customer
+            [
+            'full_name'=>'hossam customer',
+            'username'=>'customer',
+            'email'=>'customer@gmail.com',
+            'password'=>Hash::make('customer'),
+            'role'=>'customer',
+            'status'=>'active'
+                ]
+        ]);
+    }
+}
